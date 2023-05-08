16647a52ab09a59b90da9cfbdbc433218a2182c2
itai432 <gelberg4322@gmail.com>
2023-05-08 19:56:34 +0300

    home page + render posts
     html and styling with functionality to render posts from db

--- client/login/dist/login.js
+++ client/login/dist/login.js
@@ -1,3 +1,14 @@
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 function handleLogin(ev) {
     try {
         ev.preventDefault();
@@ -21,8 +32,9 @@ function handleLogin(ev) {
             .then(function (data) {
             var ok = data.ok;
             if (ok) {
-                //add herf to the main page//
-                window.location.href = "";
+                var _a = data.userDB, password_1 = _a.password, currentUser = __rest(_a, ["password"]);
+                localStorage.setItem("currentUser", JSON.stringify(currentUser));
+                window.location.href = "http://localhost:3000/homePage/index.html";
             }
             console.log(data);
         })["catch"](function (error) {
--- dist/server.js
+++ dist/server.js
@@ -20,6 +20,8 @@ app.use(express_1["default"].json());
 app.use(express_1["default"].static("./client"));
 var usersRoute_1 = require("./API/users/usersRoute");
 app.use('/api/users', usersRoute_1["default"]);
+var postsRoute_1 = require("./API/posts/postsRoute");
+app.use('/api/posts', postsRoute_1["default"]);
 app.listen(3000, function () {
     console.log("server listen on port 3000");
 });
